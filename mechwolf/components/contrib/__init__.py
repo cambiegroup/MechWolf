4a57dcb4fd22a9e36e8a0b7e1271732f1430a2ec
Dario Cambié <dario.cambie@glasgow.ac.uk>
2021-11-04 17:21:50 +0100

    Reduce change scoope

--- mechwolf/components/contrib/__init__.py
+++ mechwolf/components/contrib/__init__.py
@@ -1,9 +1,6 @@
-from .. import _ureg
-
 from .arduino import ArduinoSensor
 from .fc203 import GilsonFC203
 from .labjack import LabJack
 from .varian import VarianPump
 from .vici import ViciValve
 from .vicipump import ViciPump
-from .knauerpump import KnauerPump
